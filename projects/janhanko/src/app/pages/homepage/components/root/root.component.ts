97283994ede78b89bfd176ae84a5d5742c886c4c
Lubomir Jezek <lj@mcrai.eu>
2019-03-17 21:18:53 +0100

    add background to homepage

--- projects/janhanko/src/app/pages/homepage/components/root/root.component.ts
+++ projects/janhanko/src/app/pages/homepage/components/root/root.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostBinding, OnDestroy, OnInit } from '@angular/core';
+import { interval, Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'janhanko-homepage',
   templateUrl: './root.component.html',
   styleUrls: ['./root.component.scss']
 })
-export class RootComponent implements OnInit {
+export class RootComponent implements OnInit, OnDestroy {
+  destroy: Subject<null> = new Subject<null>();
+  backgrounds: string[] = [
+    'muj-merch.jpg',
+    'studio-usp.jpg',
+    'urban-sense.jpg',
+    'us-academy.jpg',
+    'usc-store.jpg',
+    'youtube.jpg'
+  ];
+
+  @HostBinding('style.background-image') background: string;
 
   constructor() { }
 
   ngOnInit() {
+    this.setBackground(0);
+
+    interval(3000)
+      .pipe(
+        takeUntil(this.destroy)
+      )
+      .subscribe((index: number) => this.setBackground(index + 1));
+  }
+
+  ngOnDestroy() {
+    this.destroy.next();
+  }
+
+  private setBackground(index: number): void {
+    this.background = `url(assets/images/${this.backgrounds[index % this.backgrounds.length]})`;
   }
 
 }

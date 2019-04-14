f2605c2393de9feee91b0d3691ef0cf1d606607a
Lubomir Jezek <lj@mcrai.eu>
2019-04-14 12:26:08 +0200

    update packages and tweak form

--- projects/janhanko/src/app/pages/cart/components/root/root.component.ts
+++ projects/janhanko/src/app/pages/cart/components/root/root.component.ts
@@ -1,19 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'janhanko-cart',
   templateUrl: './root.component.html',
   styleUrls: ['./root.component.scss']
 })
-export class RootComponent implements OnInit {
-
+export class RootComponent implements OnInit, OnDestroy {
+  destroy: Subject<null> = new Subject<null>();
   form: FormGroup = new FormGroup({
     test: new FormControl()
   });
+  addressCopy: FormControl = new FormControl(false);
 
   constructor() { }
 
-  ngOnInit() { }
+  ngOnInit() {
+    this.addressCopy.valueChanges
+      .pipe(
+        takeUntil(this.destroy)
+      )
+      .subscribe((value: boolean) => {
+        if (value) {
+          this.copyDeliveryAddress();
+        } else {
+          this.resetDeliveryAddress();
+        }
+      });
+  }
+
+  ngOnDestroy() {
+    this.destroy.next();
+  }
+
+  private copyDeliveryAddress(): void { }
+
+  private resetDeliveryAddress(): void { }
 
 }
